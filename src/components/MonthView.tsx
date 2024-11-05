53bc4e4efb18902b696e5d1e479314d78b3994a6
dihuynh@outlook.sg <dihuynh@outlook.sg>
2024-11-05 16:26:10 +0700

    fix: delete tooltip in month view

--- src/components/MonthView.tsx
+++ src/components/MonthView.tsx
@@ -22,40 +22,26 @@ const MonthView: React.FC<MonthViewProps> = ({ startOfMonth, onEventClick, onDat
     return (
       <ul>
         {dailyEvents.map((event) => (
-          <Tooltip
-            color="#3391fc"
-            key={event.id}
-            title={
-              <div>
-                <p>
-                  {event.title ? event.title : '(Không có tiêu đề)'} ({dayjs(event.start).format('hh:mm A')} -{' '}
-                  {dayjs(event.end).format('hh:mm A')} )
-                </p>
-              </div>
-            }
-            placement="left"
+          <li
+            className="flex gap-2"
+            onClick={(e) => {
+              onEventClick(event)
+              e.stopPropagation()
+            }}
           >
-            <li
-              className="flex gap-2"
-              onClick={(e) => {
-                onEventClick(event)
-                e.stopPropagation()
+            <div
+              style={{
+                backgroundColor: event.colorTag ? event.colorTag : '#3b82f6',
               }}
-            >
-              <div
-                style={{
-                  backgroundColor: event.colorTag ? event.colorTag : '#3b82f6',
-                }}
-                className="rounded-full w-2 h-2 mt-2"
-              ></div>
-              <div className="flex flex-col">
-                <span>{!event.title ? '(Không có tiêu đề)' : event.title}</span>
-                <span className="text-xs text-gray-500">
-                  {dayjs(event.start).format('hh:mm A')} - {dayjs(event.end).format('hh:mm A')}
-                </span>
-              </div>
-            </li>
-          </Tooltip>
+              className="rounded-full w-2 h-2 mt-2"
+            ></div>
+            <div className="flex flex-col">
+              <span>{!event.title ? '(Không có tiêu đề)' : event.title}</span>
+              <span className="text-xs text-gray-500">
+                {dayjs(event.start).format('hh:mm A')} - {dayjs(event.end).format('hh:mm A')}
+              </span>
+            </div>
+          </li>
         ))}
       </ul>
     )
