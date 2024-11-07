c979acc840f49b6e4240b41b62632837a5af8ab6
dihuynh@outlook.sg <dihuynh@outlook.sg>
2024-11-07 23:13:39 +0700

    fix: timeline

--- src/components/CurrentTimeIndicator.tsx
+++ src/components/CurrentTimeIndicator.tsx
@@ -12,7 +12,7 @@ export const CurrentTimeIndicator: React.FC<CurrentTimeIndicatorProps> = ({ curr
     <Tooltip title={`Thời gian hiện tại: ${currentHour}:${currentMinutes < 10 ? `0${currentMinutes}` : currentMinutes}`}>
       <div className="absolute left-0 w-full h-0.5 bg-red-500 flex items-center z-[9]" style={{ top: `${currentPosition}%` }}>
         <span className="absolute left-[-60px] text-xs text-red-500">
-          {`${currentHour % 12 || 12}:${currentMinutes < 10 ? `0${currentMinutes}` : currentMinutes} ${currentHour >= 12 ? 'PM' : 'AM'}`}
+          {currentHour}:{currentMinutes < 10 ? `0${currentMinutes}` : currentMinutes}
         </span>
       </div>
     </Tooltip>
